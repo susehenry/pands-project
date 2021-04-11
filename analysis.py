245d6e506a9189496be08dc393c3805c3ad6d7c6
suseh <g00398260@gmit.ie>
2021-04-11 19:50:37 +0100

    importing libraries and dataset, readme also updated

--- analysis.py
+++ analysis.py
@@ -1,1 +1,30 @@
-#Analysis of Iris Data Set
+# Analysis of Iris Data Set
+# outputs a summary of each variable to a single text file, 
+# saves a histogram of each variable to png files, and 
+# outputs a scatter plot of each pair of variables. 
+
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+import seaborn as sns
+
+sns.set()
+#this function allows you the ability to use Seaborn default colors, 
+# and Seaborn’s other styling techniques.
+
+#using pandas to read .data files
+irdata =  pd.read_csv('iris.data', sep=",")
+
+# setting the variables in the dataset
+irdata.columns = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'species']
+
+#test first 10 data sets print
+print(irdata.head (10))
+
+
+
+
+
